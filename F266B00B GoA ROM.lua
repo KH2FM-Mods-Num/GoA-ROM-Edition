5afcb881ae1ac0b3de97288711ef434d0bb1fdf8
1234567890num <53823593+1234567890num@users.noreply.github.com>
2022-03-31 14:08:44 +0700

    Version Update

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -7,7 +7,7 @@ LUAGUI_AUTH = 'SonicShadowSilver2 (Ported by Num)'
 LUAGUI_DESC = 'A GoA build for use with the Randomizer. Requires ROM patching.'
 
 function _OnInit()
-local VersionNum = 'GoA Version 1.52.14'
+local VersionNum = 'GoA Version 1.53.0'
 if (GAME_ID == 0xF266B00B or GAME_ID == 0xFAF99301) and ENGINE_TYPE == "ENGINE" then --PCSX2
 	if ENGINE_VERSION < 3.0 then
 		print('LuaEngine is Outdated. Things might not work properly.')
