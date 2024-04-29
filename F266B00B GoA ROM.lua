356e490f45f8570b4ea90953297104addab83529
1234567890num <53823593+1234567890num@users.noreply.github.com>
2024-04-30 02:02:41 +1000

    TWtNW Visit Locks

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -352,12 +352,18 @@ if true then
 	--Bone Fist
 	VisitLock(Save+0x35B4, 1, Save+0x1E56, 0x08) --NM_INIT
 	VisitLock(Save+0x35B4, 2, Save+0x1C94, 0x40) --ZZ_NM_CHECK_GOA
+	--Royal Summons (DUMMY 13)
+	VisitLock(Save+0x365D, 1, Save+0x1E12, 0x08) --DC_INIT
+	VisitLock(Save+0x365D, 2, Save+0x1C94, 0x40) --ZZ_DC_CHECK_GOA
 	--Skill and Crossbones
 	VisitLock(Save+0x35B6, 1, Save+0x1E99, 0x04) --CA_INIT
 	VisitLock(Save+0x35B6, 2, Save+0x1C94, 0x80) --ZZ_CA_CHECK_GOA
 	--Identity Disk
 	VisitLock(Save+0x35C2, 1, Save+0x1EB5, 0x20) --TR_INIT
 	VisitLock(Save+0x35C2, 2, Save+0x1C95, 0x01) --ZZ_TR_CHECK_GOA
+	--Way to the Dawn
+	VisitLock(Save+0x3642, 1, Save+0x1C95, 0x02) --ZZ_EH_CHECK_1_GOA
+	VisitLock(Save+0x3642, 2, Save+0x1C95, 0x04) --ZZ_EH_CHECK_2_GOA
 else --Remove the item requirements
 	--Namine's Sketches
 	VisitLock(Save+0x3642, 0, Save+0x1CD0, 0x01) --TT_START_1
@@ -387,12 +393,18 @@ else --Remove the item requirements
 	--Bone Fist
 	VisitLock(Save+0x35B4, 0, Save+0x1E56, 0x08) --NM_INIT
 	VisitLock(Save+0x35B4, 0, Save+0x1C94, 0x40) --ZZ_NM_CHECK_GOA
+	--Royal Summons (DUMMY 13)
+	VisitLock(Save+0x365D, 0, Save+0x1E12, 0x08) --DC_INIT
+	VisitLock(Save+0x365D, 0, Save+0x1C94, 0x40) --ZZ_DC_CHECK_GOA
 	--Skill and Crossbones
 	VisitLock(Save+0x35B6, 0, Save+0x1E99, 0x04) --CA_INIT
 	VisitLock(Save+0x35B6, 0, Save+0x1C94, 0x80) --ZZ_CA_CHECK_GOA
 	--Identity Disk
 	VisitLock(Save+0x35C2, 0, Save+0x1EB5, 0x20) --TR_INIT
 	VisitLock(Save+0x35C2, 0, Save+0x1C95, 0x01) --ZZ_TR_CHECK_GOA
+	--Way to the Dawn
+	VisitLock(Save+0x3642, 0, Save+0x1C95, 0x02) --ZZ_EH_CHECK_1_GOA
+	VisitLock(Save+0x3642, 0, Save+0x1C95, 0x04) --ZZ_EH_CHECK_2_GOA
 
 	--Disable GoA Visit Skip
 	--BitOr(Save+0x1CED,0x01) --TT_MISTERY_SKIP_GOA
