d943aa022070e0bcf776bdf77e679579e3d3b6ad
1234567890num <53823593+1234567890num@users.noreply.github.com>
2024-04-30 02:10:57 +1000

    Bugfixes

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -179,7 +179,7 @@ function VisitLock(ItemAddress, RequiredCount, Address, Bit)
 	local ItemCount = ReadByte(ItemAddress)
 	if ItemCount < RequiredCount then
 		BitNot(Address, Bit)
-	elseif ReadByte(Address) & Bit != Bit
+	elseif ReadByte(Address) & Bit != Bit then
 		BitOr(Address, Bit)
 	end
 end
